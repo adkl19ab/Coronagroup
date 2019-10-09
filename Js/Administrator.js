be41255f61ac28df746af3e2c892c82816bfe25f
JohanIsak <johanlundtoft@gmail.com>
2019-10-09 15:58:34 +0200

    Admin class done

---
+++ Js/Administrator.js
@@ -0,0 +1,12 @@
+
+class administrator {
+    constructor (name, email, usertype,) {
+        this.name = name;
+        this.email = email;
+        this. usertype = usertype;
+    }
+}
+
+var administrator1 = new administrator("Isak", "Johanlundtoft@Gmail.com","0");
+
+console.log(administrator1);
