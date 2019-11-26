68a351f437cc569c3eb5de0d372389a09162f87d
zPaddyz <mian19ah@student.cbs.dk>
2019-11-26 14:23:04 +0100

    hh

--- test/booking.js
+++ test/booking.js
@@ -1,6 +1,8 @@
 let appointments = [];
 let testest = [];
 
+
+
 class appointment {
     constructor (nameOfClient, nameOfConsultant, dateOfAppointment, startTimeOfAppointment, endTimeOfAppointment){
         this.nameOfClient = nameOfClient;
@@ -11,6 +13,20 @@ class appointment {
     }
 }
 
+currentClient = JSON.parse(localStorage.getItem('Brugere'));
+
+
+function book() {
+    for (let i = 0; i < currentClient.length; i++) {
+        if (currentClient[i].Online === true) {
+           this.currentClient[i].name = currentClient[i].clientName;
+            appointments.push(new appointment(currentClient[i].name,"","","",""));
+            localStorage.setItem('Appointment', JSON.stringify(appointments));
+            alert('Aftale oprettet');
+        }
+    }
+
+}
 function testBooking() {
     var testTid = document.getElementById('apptTime').value;
 
