47f76c678fbf0fdc2e632683a9f669d78c2f8f2d
JohanIsak <johanlundtoft@gmail.com>
2019-11-27 21:02:24 +0100

    Opsat dummy for bookings, så der kan videreudvikles på showuser og booking-oversigt mens bookingsystemet bliver arbejdet på.

--- test/booking.js
+++ test/booking.js
@@ -1,35 +1,37 @@
+var currentClient = JSON.parse(localStorage.getItem('Brugere'));
 let appointments = [];
-let testest = [];
 
 
 
 class appointment {
-    constructor (nameOfClient, nameOfConsultant, dateOfAppointment, startTimeOfAppointment, endTimeOfAppointment){
+    constructor(nameOfClient, nameOfConsultant, dateOfAppointment, startTimeOfAppointment, endTimeOfAppointment) {
         this.nameOfClient = nameOfClient;
         this.nameOfConsultant = nameOfConsultant;
         this.dateOfAppointment = dateOfAppointment;
         this.startTimeOfAppointment = startTimeOfAppointment;
         this.endTimeOfAppointment = endTimeOfAppointment;
     }
+
+    promptAlertBooking() {
+        alert(`Thank you ${this.nameOfClient}, your booking with ${this.nameOfConsultant} on ${this.dateOfAppointment} has been registered`);
+    }
 }
 
-currentClient = JSON.parse(localStorage.getItem('Brugere'));
 
-
-function book() {
+function addBooking() {
     for (let i = 0; i < currentClient.length; i++) {
         if (currentClient[i].Online === true) {
            this.currentClient[i].name = currentClient[i].clientName;
-            appointments.push(new appointment(currentClient[i].name,"","","",""));
+           let newAppointment = new appointment(
+               currentClient[i].name,
+               "",
+               document.getElementById('apptDate').value,
+               document.getElementById('apptTimeStart').value,
+               document.getElementById('apptTimeEnd').value);
+
+            appointments.push(newAppointment);
             localStorage.setItem('Appointment', JSON.stringify(appointments));
-            alert('Aftale oprettet');
+            newAppointment.promptAlertBooking();
         }
     }
-
-}
-function testBooking() {
-    var testTid = document.getElementById('apptTime').value;
-
-    appointments.push(testTid);
-
 }
