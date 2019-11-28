d81aaaa4534b15f76fceec52a44574696f506f55
carolinehertz <cahe19ad@student.cbs.dk>
2019-11-28 12:57:15 +0100

    Brugerside update

--- Logget_ind/Brugerside.js
+++ Logget_ind/Brugerside.js
@@ -4,10 +4,26 @@ function bruger() {
 
     for (let i = 0; i < currentClient.length; i++) {
         if (currentClient[i].Online === true) {
-            var name = this.currentClient[i].clientName
-            var email = this.currentClient[i].clientEmail
+            var name = this.currentClient[i].clientName;
+            var email = this.currentClient[i].clientEmail;
             document.getElementById("Brugerinfo").innerHTML= [name, email];
-        }}}
-function viewAppt(){
+        }
+    }
+}
 
+
+function viewAppt(){
+    currentClient = JSON.parse(localStorage.getItem('Appointment'));
+
+    for (let i = 0; i < currentClient.length; i++) {
+        if (currentClient[i].Online === true) {
+            var date = this.appointments[i].dateOfAppointment;
+            var start = this.appointments[i].startTimeOfAppointment;
+            var end = this.appointments[i].endTimeOfAppointment;
+            var consultant = this.appointments[i].nameOfConsultant;
+            document.getElementById("Apptinfo").innerHTML = [consultant, date, start, end];
+
+
+        }
+    }
 }
