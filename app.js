fb2867a452f8f947d28f0c969051470d18601a65
osfj19ab <osfj19ab@student.cbs.dk>
2020-04-09 16:58:49 +0200

    trying to connect to workbench

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mysql = require('mysql');
+
 
 app.use(morgan('combined'));
+
+app.get('/users/:idUSERS', (req, res)=>{
+    console.log("Fetching user with idUSERS:" + req.params.idUSERS)
+
+    const connection = mysql.createConnection({
+        //change to your own host
+        host: 'DESKTOP-QJC3050',
+        user: 'root',
+        // change for your own database
+        database: 'Local instance MySQL80'
+    })
+connection.query("SELECT * FROM projekt2020.tables.users", (err, rows, fields) =>{
+    console.log("I think we fetched users successfully")
+    res.json(rows)
+})
+   // res.end();
+})
 
 
 app.get("/", (req, res) => {
