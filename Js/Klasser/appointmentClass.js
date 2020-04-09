db9f1a84df4e3e13ca8b43e828217bbf17bd2607
JohanIsak <johanlundtoft@gmail.com>
2020-04-09 13:51:13 +0200

    Tilføjet første trin til nye klasser

---
+++ Js/Klasser/appointmentClass.js
@@ -0,0 +1,11 @@
+
+
+export class Appointment {
+    constructor(nameOfClient, nameOfConsultant, dateOfAppointment, startTimeOfAppointment, endTimeOfAppointment) {
+        this.nameOfClient = nameOfClient;
+        this.nameOfConsultant = nameOfConsultant;
+        this.dateOfAppointment = dateOfAppointment;
+        this.startTimeOfAppointment = startTimeOfAppointment;
+        this.endTimeOfAppointment = endTimeOfAppointment;
+    }
+}
